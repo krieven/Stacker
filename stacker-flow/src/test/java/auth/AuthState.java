b36b52a64552c1a99835a073fe4a0a48fb08d78a
vitaly dmitriev <vitalydmitriev1970@gmail.com>
2021-04-03 00:38:08 +0300

    clean up after merging

--- stacker-flow/src/test/java/auth/AuthState.java
+++ stacker-flow/src/test/java/auth/AuthState.java
@@ -3,12 +3,10 @@ package auth;
 import stacker.common.JsonParser;
 import stacker.flow.FlowContext;
 import stacker.flow.QuestionState;
-import stacker.flow.StateExits;
 import stacker.flow.TheContract;
 
 public class AuthState extends QuestionState<AuthQuestion, AuthAnswer, AuthSupport, AuthResources, AuthState.exits> {
 
-    @StateExits
     public enum exits {
         FORWARD, BACKWARD
     }
